ce9bc47aabd514535c6389874fed8b4d8a9067be
gnstjdwh <gnsgns0912@naver.com>
2021-09-18 15:43:56 +0900

    패키지 push

---
+++ src/main/java/com/spring/controller/PremiereController.java
@@ -0,0 +1,10 @@
+package com.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/premiere/info")
+public class PremiereController {
+	
+}
