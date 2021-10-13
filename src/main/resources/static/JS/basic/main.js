e6420e912c49ae5cb4644b00f6c5ea22858482fe
Nit4239 grand2620@naver.com <Nit4239 grand2620@naver.com>
2021-10-13 15:31:34 +0900

    css 추가
    header,movieinfo

---
+++ src/main/resources/static/JS/basic/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	
+	$('.post-wrapper1').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+	nextArrow:$('.next'),
+  	prevArrow:$('.prev'),
+});
+});
